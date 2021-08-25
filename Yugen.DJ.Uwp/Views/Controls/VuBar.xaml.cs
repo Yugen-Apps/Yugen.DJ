4cc8304e5b9b8a09fa0b99498b77c944632e963d
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2021-08-25 10:08:36 +0200

    Fix UI and solution

---
+++ Yugen.DJ.Uwp/Views/Controls/VuBar.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Windows.UI.Xaml.Controls;
+using Yugen.DJ.Uwp.ViewModels;
+using Yugen.Toolkit.Uwp.Audio.Services.Abstractions;
+
+namespace Yugen.DJ.Uwp.Views.Controls
+{
+    public sealed partial class VuBar : UserControl
+    {
+        public VuBar()
+        {
+            this.InitializeComponent();
+
+            DataContext = App.Current.Services.GetService<VuBarViewModel>();
+        }
+
+        public Side Side
+        {
+            set => ViewModel.Side = value;
+        }
+
+        private VuBarViewModel ViewModel => (VuBarViewModel)DataContext;
+    }
+}
